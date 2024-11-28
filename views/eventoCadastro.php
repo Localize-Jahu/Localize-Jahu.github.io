ca93ff1bdde0259260cebd7e69ef5fa66aa9149b
Raissa Araujo <raissaa520@gmail.com>
2024-11-28 14:32:02 -0300

    update: evento evento controller

--- views/eventoCadastro.php
+++ views/eventoCadastro.php
@@ -13,7 +13,8 @@
 
                 <div class="nome">
                     <h2>Nome do Evento:</h2>
-                    <input type="text" name="txt_nome" class="nome_evento">
+                    <input type="text" name="titulo" id="titulo">
+			        <div style="color:red;font-size:11px;"><?php echo $msg[0]; ?></div>
                 </div>
 
 
@@ -26,29 +27,39 @@
                         <div id="local1">
                             <div id="cep">
                                 <h3>CEP:</h3>
-                                <input type="text" name="txt_cep" class="local_cep">
+                                <input type="text" name="cep" id="cep">
+			                    <div style="color:red;font-size:11px;"><?php echo $msg[1]; ?></div>
                             </div>
                             <div id="bairro">
                                 <h3>Bairro:</h3>
-                                <input type="text" name="txt_bairro" class="local_bairro">
+                                <input type="text" name="bairro" id="bairro">
+			                    <div style="color:red;font-size:11px;"><?php echo $msg[2]; ?></div>
                             </div>
                         </div>
                         <div id="local2">
-                            <div id="rua">
-                                <h3>Rua:</h3>
-                                <input type="text" name="txt_rua" class="local_rua">
+                            <div id="logradouro">
+                                <h3>Logradouro:</h3>
+                                <input type="text" name="logradouro" id="logradouro">
+			                    <div style="color:red;font-size:11px;"><?php echo $msg[3]; ?></div>
                             </div>
 
-                            <div id="numero">
-                                <h3> Numero: </h3>
-                                <input type="number" name="txt_numero" class="local_numero">
+                            <div id="cidade">
+                                <h3>Cidade:</h3>
+                                <input type="text" name="cidade" id="cidade">
+                                <div style="color:red;font-size:11px;"><?php echo $msg[4]; ?></div>
+                            </div>
+
+                            <div id="uf">
+                                <h3>UF:</h3>
+                                <input type="text" name="uf" id="uf">
+			                    <div style="color:red;font-size:11px;"><?php echo $msg[5]; ?></div>
                             </div>
                         </div>
                     </div>
                 </div>
 
                 <!--Data do Evento-->
-                <h2>Data do Evento</h2>
+                <!-- <h2>Data do Evento</h2>
                 <div id="data">
                     <div id="inicio-data">
                         <h3>Inicio</h3>
@@ -58,11 +69,11 @@
                         <h3>Fim</h3>
                         <input type="date" name="txt_data">
                     </div>
-                </div>
+                </div> -->
 
 
                 <!--Hora do Evento-->
-                <h2>Hora do Evento</h2>
+                <!-- <h2>Hora do Evento</h2>
                 <div id="hora">
                     <div id="inicio-hora">
                         <h3>Inicio</h3>
@@ -72,34 +83,38 @@
                         <h3>Fim</h3>
                         <input type="time" name="txt_hora">
                     </div>
-                </div>
+                </div> -->
 
 
                 <!--Imagem do Evento-->
                 <div class="imagem">
                     <h2>Imagem do Evento</h2>
                     <!--Insira a Imagem-->
-                    <div class="label-insert">
-                        <h5>Insira a imagem</h5>
-                    </div>
+                    <input type="file" name="imagem" id="imagem" onchange="mostrar(this)">
+                    <div style="color:red;font-size:11px;"><?php echo $msg[6]; ?></div>
                     <!-- Botão Enviar Imagem-->
-                    <div class="center">
-                        <button type="reset">
-                            Enviar Imagem
-                        </button>
-                    </div>
                 </div>
 
                 <!--Descrição do Evento-->
-
                 <div class="label-50">
                     <h2>Descrição do Evento:</h2>
-                    <div class="area">
-                        <textarea name="txt_area" rows="10" cols="50"></textarea>
-                    </div>
+                    <input type="text" name="descricao" id="descricao">
+			        <div style="color:red;font-size:11px;"><?php echo $msg[7]; ?></div>
                 </div>
                 <br>
                 <br>
+                <!-- Categoria -->
+                <label for="categoria">Categoria:</label>
+			<select name="categoria" id="categoria">
+				<option value="0">Escolha uma categoria: </option>
+				<?php
+				foreach ($retorno as $dado) {
+					echo "<option value='{$dado->id_categoria}'>{$dado->descritivo}</option>";
+				}
+				?>
+			</select>
+			<div style="color:red;font-size:11px;"><?php echo $msg[8]; ?></div>
+			<br><br>
                 <!--Botão Enviar-->
                 <div class="center">
                     <button type="reset">
