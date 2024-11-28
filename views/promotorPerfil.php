8af7cbba6b0acd040680a2e0000f95dd93f9ab8a
Raissa Araujo <raissaa520@gmail.com>
2024-11-28 13:43:09 -0300

    update: evento promotor

--- views/promotorPerfil.php
+++ views/promotorPerfil.php
@@ -1,10 +1,4 @@
-<?php
 
-echo "<pre>";
-var_dump($retorno);
-echo "</pre>";
-
-?>
 <main>
         <article>
 
@@ -21,18 +15,16 @@ echo "</pre>";
 
                
                 <!--Nome Publico-->
-                <form action="/localize-jahu/promotor_perfil" class="info" enctype="multipart/form-data">
-                <input type="hidden" name="idpromotor" value="<?php echo $retorno->id_promotor;?>" >
 
                     <div class="nome">
                             <h2>Nome Publico</h2>
-                           <p><?php echo $retorno->nome_publico; ?></p>    
+                           <p><?php echo $retorno[0]->nome_publico; ?></p>    
                     </div>
 
                     <!-- Biografia -->
                     <div class="label-50">
                         <h2>Biografia</h2>
-                        <p><?php echo $retorno->biografia; ?></p>
+                        <p><?php echo $retorno[0]->biografia; ?></p>
                     </div>
 
                     <!-- Redes Sociais -->
@@ -40,44 +32,24 @@ echo "</pre>";
                         <h2>Minhas Redes Sociais</h2>
                         <div class="website">
                         <h3 class="tit-website">Website</h3>
-                        <p><?php echo $retorno->website; ?></p>
+                        <p><?php echo $retorno[0]->website; ?></p>
                 </div>
            
                 <div class="telefone">
                 <h3 class="tit-tel">Telefone</h3>
-                <p><?php echo $retorno->telefone_contato; ?></p>
+                <p><?php echo $retorno[0]->telefone_contato; ?></p>
             </div>
                 <div class="email">
                 <h3 class="tit-email">Email</h3>
-                <p><?php echo $retorno->email_contato; ?></p>
+                <p><?php echo $retorno[0]->email_contato; ?></p>
             </div>
             </div>
             
 
-                        <!--Botão Enviar-->
-                        <!-- <div class="center">
-                            <h2>Deseja desativar seu perfil de Promotor?</h2>
-                            <button type="reset">
-                                Desativar Perfil Promotor 
-                            </button>
-                        </div> -->
+                    
 
-                        <!--Botão Gerenciar Eventos-->
-                        <!-- <h2>Gerenciar Eventos</h2>
-                        <div class="gerenciar">
-                        <div class="criar">
-                            <button class="criare" type="reset">
-                                Criar Eventos
-                            </button>
-                        </div>
-
-                        <div class="editar">
-                            <button class="editar" type="reset">
-                                Editar Eventos
-                            </button>
-                        </div>
-
-                    </div> -->
+                        <a href="/localize-jahu/editar_perfil">Editar</a>
+                    </div>
                 </form>
             </div>
 
