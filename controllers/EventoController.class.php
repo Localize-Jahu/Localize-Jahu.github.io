c1ac8e75fa87a040247ea1cd4f27ec40af8a8a32
AlissaGabriel <alissagabriel14@gmail.com>
2024-11-28 09:19:12 -0300

    update: evento

--- controllers/EventoController.class.php
+++ controllers/EventoController.class.php
@@ -12,18 +12,6 @@ class EventoController
         require_once "views/rodape.html";
     }
 
-    public function exibir()
-    {
-
-        $titulo = ' - Evento';
-        $style = array("assets/styles/styleEventoExibir.css");
-        $script = array();
-
-        require_once "views/cabecalho.php";
-        require_once "views/eventoExibir.html";
-        require_once "views/rodape.html";
-    }
-
     public function calendario()
     {
 
