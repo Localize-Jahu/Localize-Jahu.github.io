3ac4eafc40beb8c8418910c2bfd1e9f3687d3746
AlissaGabriel <alissagabriel14@gmail.com>
2024-11-30 16:28:18 -0300

    add ocorrencia parcial no editEvento

--- views/editEvento.php
+++ views/editEvento.php
@@ -11,14 +11,15 @@
                 <div style="color:red;font-size:11px;"><?php echo $msg[7]; ?></div>
                 <br>
                 <img src="uploads/<?php echo $retorno[0]->imagem; ?>" id="img" width="170" height="100">
-                
+
             </div>
             <div class="conjunto">
                 <div class="titulo">
-                    <label for="titulo" class="logradouro">Titulo: </label>
+                    <label for="titulo" class="titulo">Titulo: </label>
                     <input type="text" name="titulo" id="titulo" value="<?php echo $retorno[0]->titulo; ?>">
                     <div style="color:red;font-size:11px;"><?php echo $msg[0]; ?></div>
                 </div>
+
                 <div class="logradouro">
                     <label for="logradouro" class="logradouro">Logradouro: </label>
                     <input type="text" name="logradouro" id="logradouro" value="<?php echo $retorno[0]->logradouro; ?>">
@@ -53,20 +54,51 @@
                     <textarea name="descricao" id="descricao" cols="30" rows="10"><?php echo $retorno[0]->descricao; ?></textarea>
                     <div style="color:red;font-size:11px;"><?php echo $msg[1]; ?></div>
                 </div>
-            </div>
-            <div class="categoria">
-                <label for="categoria">Categoria:</label>
-                <select name="categoria" id="categoria">
-                    <option value="0">Escolha uma categoria:</option>
-                    <?php
-                    $categorias = (new EventoDAO())->buscarTodasCategorias();
-                    foreach ($categorias as $categoria) {
-                        $selected = ($categoria->id_categoria == $retorno[0]->id_categoria) ? 'selected' : '';
-                        echo "<option value='{$categoria->id_categoria}' {$selected}>{$categoria->descritivo}</option>";
+                <?php
+                if (!empty($retorno)) {
+                    foreach ($retorno as $id => $ocorrencia) {
+                ?>
+                        <div class="container quebra" id="base-dia">
+                            <div class="container div-dia">
+                                <label for="data<?php echo $id; ?>">Data:</label>
+                                <input type="date" name="data[<?php echo $id; ?>]" id="data<?php echo $id; ?>" value="<?php echo date('Y-m-d', strtotime($ocorrencia->dia)); ?>" onchange="desativarAlerta()">
+                            </div>
+
+                            <div class="container div-dia">
+                                <label for="hora_inicio<?php echo $id; ?>">Hora de Início:</label>
+                                <input type="time" name="hora_inicio[<?php echo $id; ?>]" id="hora_inicio<?php echo $id; ?>" value="<?php echo $ocorrencia->hora_inicio; ?>" onchange="desativarAlerta()">
+                            </div>
+
+                            <div class="container div-dia">
+                                <label for="hora_termino<?php echo $id; ?>">Hora de término:</label>
+                                <input type="time" name="hora_termino[<?php echo $id; ?>]" id="hora_termino<?php echo $id; ?>" value="<?php echo $ocorrencia->hora_termino; ?>" onchange="desativarAlerta()">
+                            </div>
+                        </div>
+                <?php
                     }
-                    ?>
-                </select>
-                <div style="color:red;font-size:11px;"><?php echo $msg[8]; ?></div>
+                }
+                ?>
+
+                <a class="botao" id="btn-mais"><img class="img-mais" src="assets/images/fechar-login.png" alt=""><label class="label-mais">Adicionar dia</label></a>
+
+                <span class="alerta" id="alerta"></span>
+
+                <div id="div-clone" class="dias-adicionados">
+                </div>
+                <div class="categoria">
+                    <label for="categoria">Categoria:</label>
+                    <select name="categoria" id="categoria">
+                        <option value="0">Escolha uma categoria:</option>
+                        <?php
+                        $categorias = (new EventoDAO())->buscarTodasCategorias();
+                        foreach ($categorias as $categoria) {
+                            $selected = ($categoria->id_categoria == $retorno[0]->id_categoria) ? 'selected' : '';
+                            echo "<option value='{$categoria->id_categoria}' {$selected}>{$categoria->descritivo}</option>";
+                        }
+                        ?>
+                    </select>
+                    <div style="color:red;font-size:11px;"><?php echo $msg[8]; ?></div>
+                </div>
             </div>
             <input type="submit" class="button">
         </form>
